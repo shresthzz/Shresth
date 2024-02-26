dd31af7cd0c32c5bc4c5a56f6a1f3438aea505cd
shresthzz <151983198+shresthzz@users.noreply.github.com>
2024-02-26 10:51:34 +0530

    Create Second.kt

---
+++ src/Second.kt
@@ -0,0 +1,13 @@
+class Second {
+}
+
+fun main()
+{
+    val numberOfCups = 30
+    val numberOfPlates = 50
+    if (numberOfCups > numberOfPlates) {
+        println("Too many cups!")
+    } else {
+        println("Not enough cups!")
+    }
+}
